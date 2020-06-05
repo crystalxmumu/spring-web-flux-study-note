bb446dc7865b897ee1bace67d20f1975f6a1c104
hk <crystalxmumu@163.com>
2020-06-05 16:07:37 +0800

    创建主类

---
+++ 01-stream-api-learning/src/main/java/StreamApiLearning.java
@@ -0,0 +1,14 @@
+/**
+ * <p></p>
+ *
+ * @author hk
+ * @version 1.0.0
+ * @date 2020-06-05 16:05
+ * @since 0.1.0
+ **/
+public class StreamApiLearning {
+
+    public static void main(String[] args) {
+
+    }
+}
